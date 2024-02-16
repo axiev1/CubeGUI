c151186274ccfdf7bee5c9c27db7a848f20fcf3a
alexx <axie09@students.ubc.ca>
2024-02-16 03:49:04 -0800

    Moved user input handling from main to CubeApp

--- src/main/ui/CubeApp.java
+++ src/main/ui/CubeApp.java
@@ -1,4 +1,77 @@
 package ui;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CubeApp {
+    private CubeHandler cubeHandler;
+    private Scanner scan;
+
+    public CubeApp() {
+        cubeHandler = new CubeHandler();
+        scan = new Scanner(System.in);
+        runApp();
+    }
+
+    private void runApp() {
+        boolean run = true;
+        String command;
+
+        cubeHandler.print();
+        while (run) {
+            System.out.println("Type \"h\" for help, \"exit\" to exit");
+            command = scan.nextLine();
+
+            if (command.equalsIgnoreCase("exit")) {
+                run = false;
+            } else {
+                processCommand(command);
+            }
+        }
+    }
+
+    private void processCommand(String command) {
+        Pattern p = Pattern.compile("^(load [\\d])");
+        Matcher load = p.matcher(command);
+
+        if (command.equalsIgnoreCase("h")) {
+            printHelpMessage();
+        } else if (command.equalsIgnoreCase("scramble")) {
+            cubeHandler.scramble();
+            cubeHandler.print();
+        } else if (command.equalsIgnoreCase("scramble string")) {
+            getScramble(scan, cubeHandler);
+        } else if (command.equalsIgnoreCase("reset")) {
+            cubeHandler.reset();
+            cubeHandler.print();
+        } else if (command.equalsIgnoreCase("save")) {
+            cubeHandler.saveCube();
+        } else if (command.equalsIgnoreCase("view")) {
+            cubeHandler.viewSavedCubes();
+        } else if (load.find()) {
+            cubeHandler.loadCube(Integer.parseInt(command.substring(5)));
+        } else {
+            cubeHandler.parseTurn(command);
+            cubeHandler.print();
+        }
+    }
+
+    private void getScramble(Scanner scan, CubeHandler cubeHandler) {
+        System.out.println("Input your scramble string: ");
+        String scrambleString = scan.nextLine();
+        cubeHandler.parseScramble(scrambleString);
+        cubeHandler.print();
+    }
+
+    private void printHelpMessage() {
+        System.out.println("To make a move, type in the name of the face to rotate it 90 degrees clockwise:");
+        System.out.println("Faces (Front, Right, Up, Left, Back, Down): F R U L B D");
+        System.out.println("Add an apostrophe to the end of the move to turn counterclockwise (e.g. F', R')");
+        System.out.println("Add a 2 to the end of the move to make a double turn (e.g. R2, D2')");
+        System.out.println("To scramble the cube, type \"scramble\"");
+        System.out.println("To input a scramble string, type \"scramble string\"");
+        System.out.println("To reset the cube, type \"reset\"");
+        System.out.println("To exit, type \"exit\"");
+    }
 }
--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -7,64 +7,6 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        CubeHandler cubeHandler = new CubeHandler();
-        Scanner scan = new Scanner(System.in);
-
-        boolean run = true;
-        cubeHandler.print();
-        while (run) {
-            System.out.println("Type \"h\" for help, \"exit\" to exit");
-            run = getUserInput(scan, cubeHandler);
-        }
-    }
-
-    @SuppressWarnings("methodlength")
-    public static boolean getUserInput(Scanner scan, CubeHandler cubeHandler) {
-        String input = scan.nextLine();
-
-        Pattern p = Pattern.compile("^(load [\\d])");
-        Matcher load = p.matcher(input);
-
-        if (input.equalsIgnoreCase("h")) {
-            printHelpMessage();
-        } else if (input.equalsIgnoreCase("exit")) {
-            return false;
-        } else if (input.equalsIgnoreCase("scramble")) {
-            cubeHandler.scramble();
-            cubeHandler.print();
-        } else if (input.equalsIgnoreCase("scramble string")) {
-            getScramble(scan, cubeHandler);
-        } else if (input.equalsIgnoreCase("reset")) {
-            cubeHandler.reset();
-            cubeHandler.print();
-        } else if (input.equalsIgnoreCase("save")) {
-            cubeHandler.saveCube();
-        } else if (input.equalsIgnoreCase("view")) {
-            cubeHandler.viewSavedCubes();
-        } else if (load.find()) {
-            cubeHandler.loadCube(Integer.parseInt(input.substring(5)));
-        } else {
-            cubeHandler.parseTurn(input);
-            cubeHandler.print();
-        }
-        return true;
-    }
-
-    public static void getScramble(Scanner scan, CubeHandler cubeHandler) {
-        System.out.println("Input your scramble string: ");
-        String scrambleString = scan.nextLine();
-        cubeHandler.parseScramble(scrambleString);
-        cubeHandler.print();
-    }
-
-    public static void printHelpMessage() {
-        System.out.println("To make a move, type in the name of the face to rotate it 90 degrees clockwise:");
-        System.out.println("Faces (Front, Right, Up, Left, Back, Down): F R U L B D");
-        System.out.println("Add an apostrophe to the end of the move to turn counterclockwise (e.g. F', R')");
-        System.out.println("Add a 2 to the end of the move to make a double turn (e.g. R2, D2')");
-        System.out.println("To scramble the cube, type \"scramble\"");
-        System.out.println("To input a scramble string, type \"scramble string\"");
-        System.out.println("To reset the cube, type \"reset\"");
-        System.out.println("To exit, type \"exit\"");
+        new CubeApp();
     }
 }
